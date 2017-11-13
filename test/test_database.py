0cce11572c7eafbf85aa5bda5bf4b87bdc4a2c8c
Zhenyun Lyu <zhenyun.lyu@postgrad.manchester.ac.uk>
2017-11-13 11:15:49 +0000

    Update test_database.py

--- test/test_database.py
+++ test/test_database.py
@@ -146,6 +146,13 @@ class TestDatabase(unittest.TestCase):
             "incorrect year in result")
         self.assertEqual(data[0][1], 2,
             "incorrect number of authors in result")
+            
+    def test_calculate_first_last(self):
+	    db = database.Database()
+	    self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
+        self.assertEqual(db.calculate_first_last("Piero Fraternali"), (0, 7))
+	    self.assertEqual(db.calculate_first_last("Stefano Ceri"), (86, 33))
+	    self.assertEqual(db.calculate_first_last("aaa"), (0, 0))
 
 if __name__ == '__main__':
     unittest.main()
