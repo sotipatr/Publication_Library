23d41bb3123233dd0c5ff4c45236e860e8277ded
Constantinos Lebesis <lebesis.c@gmail.com>
2017-11-19 23:20:27 +0000

    fixes in test_sorting

--- test/test_database.py
+++ test/test_database.py
@@ -167,54 +167,33 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(db.calculate_first_last_sole(), (('Author', 'First Author', 'Last Author', 'Sole Author'),
                                                           [[u'AUTHOR', 0, 0, 9], [u'AUTHOR1', 0, 0, 1]]))
 
-    '''def test_sorting(self):
+    def test_sorting(self):
         db = database.Database()
         self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
 
         #Test cases fo publications summary
         data = db.get_publication_summary()
-        #Assert for non empty
-        self.assertTrue(views.sorting(data, 1) != 0)
-        self.assertTrue(views.sorting(data, 2) != 0)
-        self.assertTrue(views.sorting(data, 3) != 0)
-        self.assertTrue(views.sorting(data, 4) != 0)
-        self.assertTrue(views.sorting(data, 5) != 0)
-        #Assert if sorting
-        self.assertEquals(views.sorting(data, 1),data)
-        self.assertEquals(views.sorting(data, 2),data)
-        self.assertEquals(views.sorting(data, 3), data)
-        self.assertEquals(views.sorting(data, 4), data)
-        self.assertEquals(views.sorting(data, 5), data)
 
-        #Test cases fo publications per year
-        data = db.get_publications_by_year()
-        #Assert for non empty
-        self.assertTrue(views.sorting(data, 1) != 0)
-        self.assertTrue(views.sorting(data, 2) != 0)
-        self.assertTrue(views.sorting(data, 3) != 0)
-        self.assertTrue(views.sorting(data, 4) != 0)
-        self.assertTrue(views.sorting(data, 5) != 0)
-        #Assert if sorting
-        self.assertEquals(views.sorting(data, 1),data)
-        self.assertEquals(views.sorting(data, 2),data)
-        self.assertEquals(views.sorting(data, 3), data)
-        self.assertEquals(views.sorting(data, 4), data)
-        self.assertEquals(views.sorting(data, 5), data)
+        #Test case1: Test sorting of column Details
+        self.assertNotEquals(data,views.sorting(data, 1)) #case: sorted
+        self.assertEquals(data,views.sorting(data, 1)) #case: unSorted
 
-        #Test cases for author totals by year
-        data = db.get_author_totals_by_year()
-        #Assert for non empty
-        self.assertTrue(views.sorting(data, 1) != 0)
-        self.assertTrue(views.sorting(data, 2) != 0)
-        self.assertTrue(views.sorting(data, 3) != 0)
-        self.assertTrue(views.sorting(data, 4) != 0)
-        self.assertTrue(views.sorting(data, 5) != 0)
-        #Assert if sorting
-        self.assertEquals(views.sorting(data, 1),data)
-        self.assertEquals(views.sorting(data, 2),data)
-        self.assertEquals(views.sorting(data, 3), data)
-        self.assertEquals(views.sorting(data, 4), data)
-        self.assertEquals(views.sorting(data, 5), data)'''
+        #Test case2: Test sorting of column conf_papers
+        self.assertNotEquals(data,views.sorting(data, 2))
+        self.assertEquals(data,views.sorting(data, 2))
+
+        #Test case3: Test sorting of column journal
+        self.assertNotEquals(data,views.sorting(data, 3))
+        self.assertEquals(data,views.sorting(data, 3))
+
+        #Test case4: Test sorting of column book
+        self.assertNotEquals(data,views.sorting(data, 4))
+        self.assertEquals(data,views.sorting(data, 4))
+
+        #Test case5: Test sorting of column book_chapter
+        self.assertNotEquals(data,views.sorting(data, 5))
+        self.assertEquals(data,views.sorting(data, 5))
+
 
     def test_searchAuthor(self):
         db = database.Database()
