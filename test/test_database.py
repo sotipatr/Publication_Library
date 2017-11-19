27135ac5c919e4c93c5a53efd937b868b5fafed9
Zhenyun Lyu <zhenyun.lyu@postgrad.manchester.ac.uk>
2017-11-19 14:53:20 +0000

    Update test_database.py

--- test/test_database.py
+++ test/test_database.py
@@ -162,7 +162,7 @@ class TestDatabase(unittest.TestCase):
                                                           [[u'AUTHOR1', 2, 0, 1], [u'AUTHOR3', 0, 0, 0],
                                                            [u'AUTHOR4', 0, 2, 0], [u'AUTHOR2', 0, 0, 1]]))
 
-        # testcase2 database = sprint-2-acceptance-3.xml
+        #testcase3 database = sprint-2-acceptance-3.xml
         self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-3.xml")))
         self.assertEqual(db.calculate_first_last_sole(), (('Author', 'First Author', 'Last Author', 'Sole Author'),
                                                           [[u'AUTHOR', 0, 0, 9], [u'AUTHOR1', 0, 0, 1]]))
