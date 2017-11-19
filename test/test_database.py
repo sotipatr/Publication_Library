fe5f86f3f73bf07126bbf4574a24f152b6d39398
LYU <zhenyun.lyu@postgrad.manchester.ac.uk>
2017-11-19 14:51:15 +0000

    TDD: Task3-step3: final test for calculate_first_last_sole function

--- test/test_database.py
+++ test/test_database.py
@@ -156,11 +156,16 @@ class TestDatabase(unittest.TestCase):
                                                           [[u'AUTHOR1', 1, 1, 0], [u'AUTHOR2', 0, 1, 0],
                                                            [u'AUTHOR3', 0, 1, 0], [u'AUTHOR4', 2, 0, 0]]))
 
-        #testcase2 database = sprint-2-acceptance-2.xml 
+        #testcase2 database = sprint-2-acceptance-2.xml
         self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-2.xml")))
         self.assertEqual(db.calculate_first_last_sole(), (('Author', 'First Author', 'Last Author', 'Sole Author'),
                                                           [[u'AUTHOR1', 2, 0, 1], [u'AUTHOR3', 0, 0, 0],
                                                            [u'AUTHOR4', 0, 2, 0], [u'AUTHOR2', 0, 0, 1]]))
+
+        # testcase2 database = sprint-2-acceptance-3.xml
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-3.xml")))
+        self.assertEqual(db.calculate_first_last_sole(), (('Author', 'First Author', 'Last Author', 'Sole Author'),
+                                                          [[u'AUTHOR', 0, 0, 9], [u'AUTHOR1', 0, 0, 1]]))
 
     '''def test_sorting(self):
         db = database.Database()
