72c234bc72f3add3431a59ebaddfa5941c7072ea
Christos Panagi <christos.panagi@postgrad.manchester.ac.uk>
2017-12-02 12:40:48 +0000

    TDD: Sprint4 - task1: acceptance test 2

--- test/test_database.py
+++ test/test_database.py
@@ -346,6 +346,7 @@ class TestDatabase(unittest.TestCase):
         db = database.Database()
         self.assertTrue(db.read(path.join(self.data_dir,"sprint4_task1.xml")))
         self.assertEquals(db.degrees_of_separation("C","D"),1)
+        self.assertEquals(db.degrees_of_separation("A","B"),0)
 
 if __name__ == '__main__':
     unittest.main()
